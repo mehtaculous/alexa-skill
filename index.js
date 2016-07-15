944eaa4e2043695f90a55b6b9f5645e6db5ef7ba
Sahil Mehta <samehta91@gmail.com>
2016-07-15 02:05:04 -0700

    Create additional intent for missing slot values and set response equal to session attributes

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ FantasyMetrix.prototype.constructor = FantasyMetrix;
 
 FantasyMetrix.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
     var speechOutput = {
-        speech: "<speak><audio src='https://s3-eu-west-1.amazonaws.com/57647285525fb836561944563.samehta91/samehta91gmail.comSiliconValley.mp3'/><break time = \"0.5s\"/>Hello and welcome to FantasyMetrix. How can I be of assistance for you today?</speak>",
+        speech: "<speak><audio src='https://s3-eu-west-1.amazonaws.com/57647285525fb836561944563.samehta91/samehta91gmail.comSiliconValley.mp3'/> <break time = \"0.618s\"/> Hello and welcome to FantasyMetrix. How can I be of assistance for you today?</speak>",
         type: "SSML"
     };
     var repromptText = "For instructions on what you can ask, please say help me.";
@@ -65,97 +65,91 @@ FantasyMetrix.prototype.eventHandlers.onLaunch = function (launchRequest, sessio
 };
 
 FantasyMetrix.prototype.intentHandlers = {
-    "GetMetricIntent": function (intent, session, response) {
-        var sessionAttributes = {};
+    "OneShotMetricIntent": function (intent, session, response) {
 
         var playerSlot = intent.slots.Player,
             playerName;
         if (playerSlot && playerSlot.value){
             playerName = playerSlot.value.toLowerCase();
-        } else {
-            getPlayerName();
+            session.attributes.playerName = playerName;
         }
 
         var metricSlot = intent.slots.Metric,
             metricName;
         if (metricSlot && metricSlot.value){
             metricName = metricSlot.value.toLowerCase();
-        } else {
-            getMetricName();
-        }
-
-        var weekSlot = intent.slots.Week,
-            weekNumber;
-        if (weekSlot && weekSlot.value){
-            weekNumber = weekSlot.value.toLowerCase();
-        } else {
-            var speechOutput; 
+            session.attributes.metricName = metricName;
         }
 
         var seasonSlot = intent.slots.Season,
             seasonNumber;
         if (seasonSlot && seasonSlot.value){
             seasonNumber = seasonSlot.value.toLowerCase();
-        } else {
-            getSeasonNumber();
+            session.attributes.seasonNumber = seasonNumber;
         }
 
-        var player;
-        if (playerName.slice(-2) === "\'s") {
-            player = playerName.slice(0, -2);
-        } else {
-            player = playerName;
+        var weekSlot = intent.slots.Week,
+            weekNumber;
+        if (weekSlot && weekSlot.value){
+            weekNumber = weekSlot.value.toLowerCase();
+            session.attributes.weekNumber = weekNumber;
         }
 
-        var metric = metricName,
-            week = weekNumber,
-            season = seasonNumber;
+        console.log("OneShotMetricIntent: " + playerName);
+        console.log("OneShotMetricIntent: " + metricName);
+        console.log("OneShotMetricIntent: " + seasonNumber);
+        console.log("OneShotMetricIntent: " + weekNumber);
 
-        console.log(player);
-        console.log(metric);
-        console.log(week);
-        console.log(season);
-
-        var speechOutput,
-            repromptOutput,
-            cardTitle,
-            cardContent;
-
-        if (Fantasy[player] && Fantasy[player][season] && Fantasy[player][season][week] && Fantasy[player][season][week][metric]) {
-            speechOutput = {
-                speech: "During " + week + " of the " + season + " season, " + player + " had " + JSON.stringify(Fantasy[player][season][week][metric]) + " " + metric,
-                type: AlexaSkill.speechOutputType.PLAIN_TEXT
-            };
-            cardTitle = metric + " for " + player + " during " + week + " of the " + season + " season";
-            cardContent = JSON.stringify(Fantasy[player][season][week][metric]) + " " + metric;
-            response.tellWithCard(speechOutput, cardTitle, cardContent);
-
-        } else if (Fantasy[player] && Fantasy[player][season] && Fantasy[player][season][metric]) {
-            speechOutput = {
-                speech: "During the " + season + " season, " + player + " had " + JSON.stringify(Fantasy[player][season][metric]) + " " + metric,
-                type: AlexaSkill.speechOutputType.PLAIN_TEXT
-            };
-            cardTitle = metric + " for " + player + " during the " + season + " season";
-            cardContent = JSON.stringify(Fantasy[player][season][metric]) + " " + metric;
-            response.tellWithCard(speechOutput, cardTitle, cardContent);
-
+        if (playerName === undefined) {
+            handleMissingPlayerRequest(intent, session, response);
+        } else if (metricName === undefined) {
+            handleMissingMetricRequest(intent, session, response);
+        } else if (seasonNumber === undefined) {
+            handleMissingSeasonRequest(intent, session, response);
         } else {
-            var speech = "I'm sorry, I currently do not know what you are asking for. What else can I help you with?";
-            speechOutput = {
-                speech: speech,
-                type: AlexaSkill.speechOutputType.PLAIN_TEXT
-            };
-            repromptOutput = {
-                speech: "Is there anything else I can help you with today?",
-                type: AlexaSkill.speechOutputType.PLAIN_TEXT
-            };
-            response.ask(speechOutput, repromptOutput);
+            getMetricRequest(intent, session, response);
         }
     },
 
+    "MissingSlotIntent": function (intent, session, response) {
+        var playerSlot = intent.slots.Player,
+            metricSlot = intent.slots.Metric,
+            seasonSlot = intent.slots.Season,
+            playerName,
+            metricName,
+            seasonNumber;
+
+        if (playerSlot && playerSlot.value) {
+            playerName = playerSlot.value.toLowerCase();
+            session.attributes.playerName = playerName;
+        } else if (session.attributes.playerName === undefined) {
+            handleMissingPlayerRequest(intent, session, response);
+        }
+
+        if (metricSlot && metricSlot.value) {
+            metricName = metricSlot.value.toLowerCase();
+            session.attributes.metricName = metricName;
+        } else if (session.attributes.metricName === undefined) {
+            handleMissingMetricRequest(intent, session, response);
+        }
+
+        if (seasonSlot && seasonSlot.value) {
+            seasonNumber = seasonSlot.value.toLowerCase();
+            session.attributes.seasonNumber = seasonNumber;
+        } else if (session.attributes.seasonNumber === undefined) {
+            handleMissingSeasonRequest(intent, session, response);
+        }
+
+        console.log("MissingPlayerIntent: " + playerSlot.value);
+        console.log("MissingMetricIntent: " + metricSlot.value);
+        console.log("MissingSeasonIntent: " + seasonSlot.value);
+
+        getMetricRequest(intent, session, response);
+    },
+
     "AMAZON.HelpIntent": function (intent, session, response) {
-        var speechText = "You can ask questions such as <break time = \"0.5s\"/> Get me Russell Wilson's passing touchdowns for week one of two thousand and fifteen? <break time = \"0.5s\"/> Now, what can I help you with?";
-        var repromptText = "You can say things like <break time = \"0.5s\"/> How many receiving yards did Julio Jones have during week two of two thousand and fifteen? <break time = \"0.5s\"/> Now, how can I help you?";
+        var speechText = "You can ask questions such as <break time = \"0.618s\"/> Get me Russell Wilson's passing touchdowns for week one of two thousand and fifteen? <break time = \"0.618s\"/> Now, what can I help you with?";
+        var repromptText = "You can say things like <break time = \"0.618s\"/> How many receiving yards did Julio Jones have during week two of two thousand and fifteen? <break time = \"0.618s\"/> Now, how can I help you?";
         var speechOutput = {
             speech: speechText,
             type: AlexaSkill.speechOutputType.PLAIN_TEXT
@@ -173,45 +167,91 @@ FantasyMetrix.prototype.intentHandlers = {
     }
 };
 
-function getPlayerName(intent, session, callback) {
-
-}
-
-function getMetricName(intent, session, callback) {
-    
-}
-
-function getWeekNumber(intent, session, callback) {
-    
-}
-
-function getSeasonNumber(intent, session, callback) {
-    
-}
-
-function buildSpeechletResponseWithoutCard(output, repromptText, shouldEndSession) {
-    return {
-        outputSpeech: {
-            type: "PlainText",
-            text: output
-        },
-        reprompt: {
-            outputSpeech: {
-                type: "PlainText",
-                text: repromptText
-            }
-        },
-        shouldEndSession: shouldEndSession
-    };
+function handleMissingPlayerRequest(intent, session, response) {
+    var speechOutput = "Please provide the name of a current player who has played at least one season in the National Football League.",
+        repromptText = "You can say something like, Russell Wilson";
+    response.ask(speechOutput, repromptText);
 };
 
-function buildResponse(sessionAttributes, speechletResponse) {
-    return {
-        version: "1.0",
-        sessionAttributes: sessionAttributes,
-        response: speechletResponse
-    };
+function handleMissingMetricRequest(intent, session, response) {
+    var player;
+    var playerName = session.attributes.playerName;
+    if (playerName.slice(-2) === "\'s") {
+        player = playerName;
+    } else {
+        player = playerName.concat("\'s");
+    }
+
+    var speechOutput = "Please provide a valid metric which correlates to " + player + " position.",
+        repromptText = "You can say something like, Passing Yards."
+    response.ask(speechOutput, repromptText);
 };
+
+function handleMissingSeasonRequest(intent, session, response) {
+    var player;
+    var playerName = session.attributes.playerName;
+    if (playerName.slice(-2) === "\'s") {
+        player = playerName.slice(0, -2);
+    } else {
+        player = playerName;
+    }
+
+    var speechOutput = "Please provide the year of a valid NFL season, ranging from two thousand and ten through two thousand and fifteen, in which " + player + " played at least one game.",
+        repromptText = "You can say something like, Two Thousand and Fifteen."
+    response.ask(speechOutput, repromptText);
+};
+
+function getMetricRequest(intent, session, response) {
+    var player;
+    var playerName = session.attributes.playerName;
+    if (playerName.slice(-2) === "\'s") {
+        player = playerName.slice(0, -2);
+    } else {
+        player = playerName;
+    }
+
+    var metric = session.attributes.metricName,
+        season = session.attributes.seasonNumber,
+        week = session.attributes.weekNumber,
+        speechOutput,
+        repromptOutput,
+        cardTitle,
+        cardContent;
+
+    console.log("getMetricRequest: " + player);
+    console.log("getMetricRequest: " + metric);
+    console.log("getMetricRequest: " + season);
+    console.log("getMetricRequest: " + week);
+
+    if (Fantasy[player] && Fantasy[player][season] && Fantasy[player][season][week] && Fantasy[player][season][week][metric]) {
+        speechOutput = {
+            speech: "During " + week + " of the " + season + " season, " + player + " had " + JSON.stringify(Fantasy[player][season][week][metric]) + " " + metric,
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        cardTitle = metric + " for " + player + " during " + week + " of the " + season + " season";
+        cardContent = JSON.stringify(Fantasy[player][season][week][metric]) + " " + metric;
+        response.tellWithCard(speechOutput, cardTitle, cardContent);
+    } else if (Fantasy[player] && Fantasy[player][season] && Fantasy[player][season][metric]) {
+        speechOutput = {
+            speech: "During the " + season + " season, " + player + " had " + JSON.stringify(Fantasy[player][season][metric]) + " " + metric,
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        cardTitle = metric + " for " + player + " during the " + season + " season";
+        cardContent = JSON.stringify(Fantasy[player][season][metric]) + " " + metric;
+        response.tellWithCard(speechOutput, cardTitle, cardContent);
+    } else {
+        var speech = "I'm sorry, I currently do not know what you are asking for. Please make sure that you are providing the name of a current player who has played at least one season in the National Football League, a valid metric correlating to that player's position, and a valid year pertaining to the NFL season ranging from two thousand and ten through two thousand and sixteen. Providing a regular season week number ranging from week one through week seventeen is optional.";
+        speechOutput = {
+            speech: speech,
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        repromptOutput = {
+            speech: "Is there anything else I can help you with today?",
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        response.ask(speechOutput, repromptOutput);
+    }
+}
 
 exports.handler = function (event, context) {
     var skill = new FantasyMetrix();
